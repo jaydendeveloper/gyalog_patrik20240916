ace37a0e64b40da15e940d3b25efdcbe5ddef516
Patrik Gyalog <contact.jaydendev@gmail.com>
2024-09-27 17:51:18 +0200

    Feladat 9- 12

--- feladat3.js
+++
@@ -1,6 +0,0 @@
-const mass_in_kg = 81.2;
-const height_in_m = 1.78;
-
-const bmi = mass_in_kg / (height_in_m * height_in_m);
-
-console.log(bmi);
--- feladat7.js
+++
@@ -1,7 +0,0 @@
-//Write a for loop that iterates through a string and prints each letter.
-
-const my_string = 'apple';
-
-for(let i = 0; i < my_string.length; i++){
-    console.log(my_string[i]);
-}
